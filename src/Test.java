f03f3adeefdee85503e89f2d9b5f7934b95e68cb
YeonjungJo <yeonjung.jo@gmail.com>
2016-02-26 22:49:42 +0900

    day20160226

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	private static final String LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
 	public static void main(String[] args) {
-		String s = "10100";
-		System.out.printf("%d\n", s.length() - s.replaceAll("[0]*$", "").length());
+		System.out.println(0 % 3);
 	}
 }
